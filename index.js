f3d16257f0594602eb265b5fe9f51d8d21657e46
Justin Woo <moomoowoo@gmail.com>
2014-12-19 16:31:24 -0500

    Make redis work with simple push whatnot

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 var Hapi = require('hapi');
 var Good = require('good');
+var redis = require('redis');
 
-var routes = require('./lib/routes');
+var initializeModel = require('./lib/model').initializeModel;
+var initializeRoutes = require('./lib/routes').initializeRoutes;
 
 var HOST = process.env.HOST || 'localhost';
 var PORT = process.env.PORT || 8000;
+
+var redisClient = redis.createClient();
+
+var model = initializeModel({
+  redisClient: redisClient
+});
+
+var routes = initializeRoutes({
+  model: model
+});
 
 var server = new Hapi.Server();
 
